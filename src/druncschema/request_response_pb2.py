956a7b39c55d5256891b43d44b7b2be86aaa6bd1
Pierre Lasorak <pierre.lasorak@gmail.com>
2023-05-10 09:29:11 -0400

    update auto gen

--- src/druncschema/request_response_pb2.py
+++ src/druncschema/request_response_pb2.py
@@ -15,7 +15,7 @@ import druncschema.token_pb2 as token__pb2
 from google.protobuf import any_pb2 as google_dot_protobuf_dot_any__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x16request_response.proto\x1a\x0btoken.proto\x1a\x19google/protobuf/any.proto\"R\n\x07Request\x12\x15\n\x05token\x18\x01 \x01(\x0b\x32\x06.Token\x12\'\n\x04\x64\x61ta\x18\x02 \x01(\x0b\x32\x14.google.protobuf.AnyH\x00\x88\x01\x01\x42\x07\n\x05_data\"S\n\x08Response\x12\x15\n\x05token\x18\x01 \x01(\x0b\x32\x06.Token\x12\'\n\x04\x64\x61ta\x18\x02 \x01(\x0b\x32\x14.google.protobuf.AnyH\x00\x88\x01\x01\x42\x07\n\x05_data\",\n\x06Stream\x12\"\n\x04\x64\x61ta\x18\x01 \x01(\x0b\x32\x14.google.protobuf.Anyb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x16request_response.proto\x1a\x0btoken.proto\x1a\x19google/protobuf/any.proto\"R\n\x07Request\x12\x15\n\x05token\x18\x01 \x01(\x0b\x32\x06.Token\x12\'\n\x04\x64\x61ta\x18\x02 \x01(\x0b\x32\x14.google.protobuf.AnyH\x00\x88\x01\x01\x42\x07\n\x05_data\"S\n\x08Response\x12\x15\n\x05token\x18\x01 \x01(\x0b\x32\x06.Token\x12\'\n\x04\x64\x61ta\x18\x02 \x01(\x0b\x32\x14.google.protobuf.AnyH\x00\x88\x01\x01\x42\x07\n\x05_data\"C\n\x06Stream\x12\x15\n\x05token\x18\x01 \x01(\x0b\x32\x06.Token\x12\"\n\x04\x64\x61ta\x18\x02 \x01(\x0b\x32\x14.google.protobuf.Anyb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'request_response_pb2', globals())
@@ -27,5 +27,5 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _RESPONSE._serialized_start=150
   _RESPONSE._serialized_end=233
   _STREAM._serialized_start=235
-  _STREAM._serialized_end=279
+  _STREAM._serialized_end=302
 # @@protoc_insertion_point(module_scope)
