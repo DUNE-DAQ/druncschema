db6cf574938c6ff7476186c0755b5fca1f6805a3
Pierre Lasorak <pierre.lasorak@gmail.com>
2023-05-10 09:28:14 -0400

    quiet please

--- generate_protos.sh
+++ generate_protos.sh
@@ -6,10 +6,10 @@ do
     FILE_NAME=$(basename -- "$FILE")
     CLASS_NAME="${FILE_NAME%.*}"
     PY_OUT_FILE_NAME="src/druncschema/${CLASS_NAME}_pb2_grpc.py"
-    echo "FILE_NAME        " $FILE_NAME
-    echo "CLASS_NAME       " $CLASS_NAME
-    echo "PY_OUT_FILE_NAME " $PY_OUT_FILE_NAME
-    echo "s/import ${CLASS_NAME}_pb2/import drunc.communication.${CLASS_NAME}_pb2/"
+    # echo "FILE_NAME        " $FILE_NAME
+    # echo "CLASS_NAME       " $CLASS_NAME
+    # echo "PY_OUT_FILE_NAME " $PY_OUT_FILE_NAME
+    # echo "s/import ${CLASS_NAME}_pb2/import drunc.communication.${CLASS_NAME}_pb2/"
     sed -i '' "/google/! s/import \(.*_pb2\)/import druncschema.\1/" ${PY_OUT_FILE_NAME}
     PY_OUT_FILE_NAME="src/druncschema/${CLASS_NAME}_pb2.py"
     sed -i '' "/google/! s/import \(.*_pb2\)/import druncschema.\1/" ${PY_OUT_FILE_NAME}
