aa8465c2c388336a15c3458f2b6190c2282dc1b8
wanyunSu <wanyun.su@cern.ch>
2025-03-12 12:13:49 +0100

    add n_session

--- python_not_for_dunedaq/druncschema/opmon_process_manager_pb2.py
+++ python_not_for_dunedaq/druncschema/opmon_process_manager_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1bopmon_process_manager.proto\x12\x13\x64unedaq.opmon.drunc\"2\n\rProcessStatus\x12\x11\n\tn_running\x18\x01 \x01(\x03\x12\x0e\n\x06n_dead\x18\x02 \x01(\x03\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1bopmon_process_manager.proto\x12\x13\x64unedaq.opmon.drunc\"E\n\rProcessStatus\x12\x11\n\tn_running\x18\x01 \x01(\x03\x12\x0e\n\x06n_dead\x18\x02 \x01(\x03\x12\x11\n\tn_session\x18\x03 \x01(\x03\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -32,5 +32,5 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'opmon_process_manager_pb2',
 if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
   _globals['_PROCESSSTATUS']._serialized_start=52
-  _globals['_PROCESSSTATUS']._serialized_end=102
+  _globals['_PROCESSSTATUS']._serialized_end=121
 # @@protoc_insertion_point(module_scope)
