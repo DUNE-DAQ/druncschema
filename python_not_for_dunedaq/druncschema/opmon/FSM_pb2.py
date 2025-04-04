2a494b34adfd3271598c9e573d9eb92fd45fc1eb
wanyunSu <wanyun.su@cern.ch>
2025-04-04 13:00:17 +0200

    final finally time type fixed

--- python_not_for_dunedaq/druncschema/opmon/FSM_pb2.py
+++ python_not_for_dunedaq/druncschema/opmon/FSM_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1b\x64runcschema/opmon/FSM.proto\x12\x13\x64unedaq.drunc.opmon\"Q\n\tFSMStatus\x12\r\n\x05state\x18\x01 \x01(\t\x12\x11\n\tsub_state\x18\x02 \x01(\t\x12\x10\n\x08in_error\x18\x03 \x01(\x08\x12\x10\n\x08included\x18\x04 \x01(\x08\")\n\x0b\x43ommandTime\x12\x1a\n\x12\x63md_execution_time\x18\x01 \x01(\x04\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1b\x64runcschema/opmon/FSM.proto\x12\x13\x64unedaq.drunc.opmon\"Q\n\tFSMStatus\x12\r\n\x05state\x18\x01 \x01(\t\x12\x11\n\tsub_state\x18\x02 \x01(\t\x12\x10\n\x08in_error\x18\x03 \x01(\x08\x12\x10\n\x08included\x18\x04 \x01(\x08\"(\n\x0b\x43ommandTime\x12\x19\n\x11\x65xecution_time_ns\x18\x01 \x01(\x04\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -34,5 +34,5 @@ if not _descriptor._USE_C_DESCRIPTORS:
   _globals['_FSMSTATUS']._serialized_start=52
   _globals['_FSMSTATUS']._serialized_end=133
   _globals['_COMMANDTIME']._serialized_start=135
-  _globals['_COMMANDTIME']._serialized_end=176
+  _globals['_COMMANDTIME']._serialized_end=175
 # @@protoc_insertion_point(module_scope)
