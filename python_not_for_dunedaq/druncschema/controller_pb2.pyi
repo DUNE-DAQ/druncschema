67a47b7ccd51242305f939ae48cc2c6b7ff7187d
James Paul Turner <james.turner@imperial.ac.uk>
2025-10-29 15:44:27 +0000

    FSMCommand.command_name as before.

--- python_not_for_dunedaq/druncschema/controller_pb2.pyi
+++ python_not_for_dunedaq/druncschema/controller_pb2.pyi
@@ -166,22 +166,22 @@ class FSMCommand(google.protobuf.message.Message):
         def HasField(self, field_name: typing.Literal["value", b"value"]) -> builtins.bool: ...
         def ClearField(self, field_name: typing.Literal["key", b"key", "value", b"value"]) -> None: ...
 
-    NAME_FIELD_NUMBER: builtins.int
+    COMMAND_NAME_FIELD_NUMBER: builtins.int
     ARGUMENTS_FIELD_NUMBER: builtins.int
     DATA_FIELD_NUMBER: builtins.int
-    name: builtins.str
+    command_name: builtins.str
     data: builtins.str
     @property
     def arguments(self) -> google.protobuf.internal.containers.MessageMap[builtins.str, google.protobuf.any_pb2.Any]: ...
     def __init__(
         self,
         *,
-        name: builtins.str = ...,
+        command_name: builtins.str = ...,
         arguments: collections.abc.Mapping[builtins.str, google.protobuf.any_pb2.Any] | None = ...,
         data: builtins.str | None = ...,
     ) -> None: ...
     def HasField(self, field_name: typing.Literal["_data", b"_data", "data", b"data"]) -> builtins.bool: ...
-    def ClearField(self, field_name: typing.Literal["_data", b"_data", "arguments", b"arguments", "data", b"data", "name", b"name"]) -> None: ...
+    def ClearField(self, field_name: typing.Literal["_data", b"_data", "arguments", b"arguments", "command_name", b"command_name", "data", b"data"]) -> None: ...
     def WhichOneof(self, oneof_group: typing.Literal["_data", b"_data"]) -> typing.Literal["data"] | None: ...
 
 global___FSMCommand = FSMCommand
