7c8077a870af60764ebe097355657d3a3c714445
James Paul Turner <james.turner@imperial.ac.uk>
2025-11-04 18:19:06 +0000

    execute_expert_command: new response msg.

--- src/druncschema/controller_pb2.pyi
+++ src/druncschema/controller_pb2.pyi
@@ -108,6 +108,39 @@ class ExecuteExpertCommandRequest(google.protobuf.message.Message):
 global___ExecuteExpertCommandRequest = ExecuteExpertCommandRequest
 
 @typing.final
+class ExecuteExpertCommandResponse(google.protobuf.message.Message):
+    DESCRIPTOR: google.protobuf.descriptor.Descriptor
+
+    TOKEN_FIELD_NUMBER: builtins.int
+    NAME_FIELD_NUMBER: builtins.int
+    DATA_FIELD_NUMBER: builtins.int
+    CHILDREN_FIELD_NUMBER: builtins.int
+    FSM_FLAG_FIELD_NUMBER: builtins.int
+    FLAG_FIELD_NUMBER: builtins.int
+    name: builtins.str
+    data: builtins.str
+    fsm_flag: global___FSMResponseFlag.ValueType
+    flag: druncschema.request_response_pb2.ResponseFlag.ValueType
+    @property
+    def token(self) -> druncschema.token_pb2.Token: ...
+    @property
+    def children(self) -> google.protobuf.internal.containers.RepeatedCompositeFieldContainer[global___ExecuteExpertCommandResponse]: ...
+    def __init__(
+        self,
+        *,
+        token: druncschema.token_pb2.Token | None = ...,
+        name: builtins.str = ...,
+        data: builtins.str = ...,
+        children: collections.abc.Iterable[global___ExecuteExpertCommandResponse] | None = ...,
+        fsm_flag: global___FSMResponseFlag.ValueType = ...,
+        flag: druncschema.request_response_pb2.ResponseFlag.ValueType = ...,
+    ) -> None: ...
+    def HasField(self, field_name: typing.Literal["token", b"token"]) -> builtins.bool: ...
+    def ClearField(self, field_name: typing.Literal["children", b"children", "data", b"data", "flag", b"flag", "fsm_flag", b"fsm_flag", "name", b"name", "token", b"token"]) -> None: ...
+
+global___ExecuteExpertCommandResponse = ExecuteExpertCommandResponse
+
+@typing.final
 class ExecuteFSMCommandRequest(google.protobuf.message.Message):
     DESCRIPTOR: google.protobuf.descriptor.Descriptor
 
--- src/druncschema/controller_pb2_grpc.py
+++ src/druncschema/controller_pb2_grpc.py
@@ -58,7 +58,7 @@ class ControllerStub(object):
         self.execute_expert_command = channel.unary_unary(
                 '/dunedaq.druncschema.controller.Controller/execute_expert_command',
                 request_serializer=druncschema_dot_controller__pb2.ExecuteExpertCommandRequest.SerializeToString,
-                response_deserializer=druncschema_dot_request__response__pb2.Response.FromString,
+                response_deserializer=druncschema_dot_controller__pb2.ExecuteExpertCommandResponse.FromString,
                 _registered_method=True)
         self.include = channel.unary_unary(
                 '/dunedaq.druncschema.controller.Controller/include',
@@ -198,7 +198,7 @@ def add_ControllerServicer_to_server(servicer, server):
             'execute_expert_command': grpc.unary_unary_rpc_method_handler(
                     servicer.execute_expert_command,
                     request_deserializer=druncschema_dot_controller__pb2.ExecuteExpertCommandRequest.FromString,
-                    response_serializer=druncschema_dot_request__response__pb2.Response.SerializeToString,
+                    response_serializer=druncschema_dot_controller__pb2.ExecuteExpertCommandResponse.SerializeToString,
             ),
             'include': grpc.unary_unary_rpc_method_handler(
                     servicer.include,
@@ -370,7 +370,7 @@ class Controller(object):
             target,
             '/dunedaq.druncschema.controller.Controller/execute_expert_command',
             druncschema_dot_controller__pb2.ExecuteExpertCommandRequest.SerializeToString,
-            druncschema_dot_request__response__pb2.Response.FromString,
+            druncschema_dot_controller__pb2.ExecuteExpertCommandResponse.FromString,
             options,
             channel_credentials,
             insecure,
